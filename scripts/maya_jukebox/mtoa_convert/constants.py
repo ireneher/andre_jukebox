3a3dbfffe742c2e4345c9a2fc24939085b3338ac
ireneher <theireneh@gmail.com>
2020-06-29 23:59:38 +0100

    mtoa_convert updated

--- scripts/maya_jukebox/mtoa_convert/constants.py
+++ scripts/maya_jukebox/mtoa_convert/constants.py
@@ -3,7 +3,13 @@ refs_dir_rel = "scenes/REFS"
 archive_dir_rel = "archive"
 textures_dir_rel = "sourceimages/SETS/buildings/Textures_1024"
 texture_mappings = {"baseColor": ("Diffuse", "sRGB"),
-                   "diffuseRoughness": ("Glossiness", "Raw"),
+                   "specularRoughness": ("Glossiness", "Raw"),
                    "specularIOR": ("ior", "Raw"),
                    "normalCamera": ("Normal", "sRGB"),
-                   "specularRoughness": ("Reflection", "Raw")}  # shader component: (texture map suffix, colourspace)
+                   "specularColor": ("Reflection", "Raw"),
+                    "transmission": ("Opacity", "Raw")}
+                   # shader component: (texture map suffix, colourspace)
+
+alpha_invert = ("specularRoughness", "specularIOR", "transmission")  # alpha is luminance, invert
+interior_shading_group = "am215_interior_V{}_SHDSG"
+interior_shop_shading_group = "am215_interior_shop_V{}_SHDSG"
